d7f9dbaf5ca3d6a15bbca92ae2d79fedcf18e1a8
Zhalok <zhalokrahman007@gmail.com>
2021-06-18 10:26:02 +0600

    updated

--- Amin/Admin.java
+++ Amin/Admin.java
@@ -39,4 +39,54 @@ public class Admin {
         writer.close();
         
     }
+    public static void main(String[] args) {
+        Admin admin = new Admin();
+       
+        while(true)
+        {
+
+        System.out.println("Hello Admin what do you want to do ? ");
+        System.out.println("Press 1 to authentify students");
+        System.out.println("Press 2 to authentify instructors");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        
+        if(x==1)
+        {
+            try{
+                
+                admin.authentify_Students();
+                System.out.println("Students are authentified now please check the student lists");
+                System.out.println("Do u want to continiue?");
+                String msg = scanner.nextLine();
+                if(msg.matches("No")||msg.matches("no"))
+                {
+                    return ;
+                }
+                
+
+            }catch(Exception e)
+            {
+                System.out.println("File not found");
+            }
+        }else if(x==2)
+        {
+            try{
+                admin.authentify_Instructors();
+                System.out.println("Instructors are authentified");
+                System.out.println("Do u want to continute?");
+                String msg = scanner.nextLine();
+                if(msg.matches("No")||msg.matches("no"))
+                {
+                    return ;
+                }
+                
+            }catch(Exception e)
+            {
+                System.out.println("File not found");
+            }
+        }
+     }
+
+    }
 }
